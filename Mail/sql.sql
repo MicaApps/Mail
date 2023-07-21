2fa2635a6b07c4f56a56d96b705cba3c4e8219dd
GaN601 <gan.n@outlook.com>
2023-07-21 23:26:40 +0800

    init ddl sql

---
+++ Mail/sql.sql
@@ -0,0 +1,43 @@
+﻿create table main.MailFolderData
+(
+    Id               NVARCHAR(255) not null
+        primary key,
+    Name             NVARCHAR(255),
+    Type             MEDIUMINT     not null,
+    IsHidden         BOOLEAN       not null,
+    TotalItemCount   INTEGER       not null,
+    MailType         MEDIUMINT     not null,
+    ParentFolderId   NVARCHAR(255),
+    ChildFolderCount INTEGER       not null
+);
+
+create table main.MailMessageContentData
+(
+    MessageId      NVARCHAR(255) not null
+        primary key,
+    Content        TEXT,
+    ContentPreview NVARCHAR(255),
+    ContentType    MEDIUMINT     not null
+);
+
+create table main.MailMessageData
+(
+    MessageId               NVARCHAR(255) not null
+        primary key,
+    InferenceClassification NVARCHAR(128),
+    FolderId                NVARCHAR(255),
+    Title                   NVARCHAR(255),
+    SentTime                DATETIME
+);
+
+create table main.MailMessageRecipientData
+(
+    Id            integer       not null
+        constraint MailMessageRecipientData_pk
+            primary key autoincrement,
+    MessageId     NVARCHAR(255) not null,
+    Name          NVARCHAR(255),
+    Address       NVARCHAR(255) not null,
+    RecipientType MEDIUMINT     not null
+);
+
