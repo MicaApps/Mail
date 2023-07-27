5ba13886e6bcc86b416f6a5c603158de9cad2c46
walterlv <xblero@gmail.com>
2023-07-27 20:29:30 +0800

    将 Mail 项目移至 src 文件夹下，以便后续采用多项目开发。

---
+++ src/MicaApps.Mail.UWP/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Mail.Extensions;
+using Mail.Services;
+using Mail.Services.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Mail.Pages
+{
+    public sealed partial class SettingsPage : Page
+    {
+        private readonly ObservableCollection<ContactModel> ContactSource = new ObservableCollection<ContactModel>();
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void SignOut(object sender, RoutedEventArgs e)
+        {
+            await App.Services.GetService<OutlookService>().SignOutAsync(null);
+        }
+
+        public async void GetEmail(object sender, RoutedEventArgs e)
+        {
+            ContactSource.Clear();
+            ContactSource.AddRange(await App.Services.GetService<OutlookService>().GetContactsAsync());
+        }
+    }
+}
