29d68cac3a372ae05109e6e36a5695a4fc903329
kengwang <atkengwang@qq.com>
2023-08-29 12:30:34 +0800

    [feat] Add Secrets

--- MicaApps.Mail.Tests/Secrets.cs
+++ MicaApps.Mail.Tests/Secrets.cs
@@ -2,7 +2,24 @@
 
 // THIS FILE SHOULD BE FILLED BY GITHUB ACTION
 // **DO NOT** COMMIT YOUR SECRETS TO ORIGIN
-public class Secrets
+public static class Secrets
 {
-    
+    public static ProtocolMailSettings ProtocolMailSettings =
+        new()
+        {
+            Host = null,
+            Port = 0,
+            UseSsl = false,
+            Username = false,
+            Password = false
+        };
+}
+
+public class ProtocolMailSettings
+{
+    public string Host;
+    public int Port;
+    public bool UseSsl;
+    public bool Username;
+    public bool Password;
 }
