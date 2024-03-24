206f592d00dc60e9ff89d74a3fcf13058ff4153d
shompinice <shomnipotence@hotmail.com>
2024-03-24 18:11:06 +0800

    1

--- src/MicaApps.Mail.UWP/Secret.cs
+++ src/MicaApps.Mail.UWP/Secret.cs
@@ -5,7 +5,7 @@
 #if DEBUG
         public const string AadClientId = "0b3dac55-dc21-442b-ace7-ccefbb5a9f80";
 #else
-        public const string AadClientId = "<Mail-AAD-Secret-Value>";
+        public const string AadClientId = "0b3dac55-dc21-442b-ace7-ccefbb5a9f80";
 #endif
     }
 }
